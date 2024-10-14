96d4cd5104132e6f4de768633d30fe0124000c5c
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-14 10:11:22 +0200

    end snack 6

--- index.js
+++ index.js
@@ -4,6 +4,50 @@
 // Stampare a schermo la bici con peso minore.
 
 
+const bici = [
+  {
+    name: "bike 1",
+    peso: 10
+  },
+  {
+    name: "bike 2",
+    peso: 13
+  },
+  {
+    name: "bike 3",
+    peso: 11
+  },
+  {
+    name: "bike 4",
+    peso: 9
+  },
+  {
+    name: "bike 5",
+    peso: 20
+  },
+  {
+    name: "bike 6",
+    peso: 17
+  },
+];
+
+let lightBike = bici[0]
+
+
+for (let i = 0; i < bici.length; i++) {
+    const bike = bici[i]
+
+    if (bike.peso < lightBike.peso) {
+        lightBike = bike
+    }
+}
+
+
+console.log(lightBike);
+
+
+
+
 // Snack 2
 // Creare un array di oggetti di squadre di calcio.
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
@@ -12,6 +56,56 @@
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 
-// Snack 3 (Bonus)
-// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+function getRandomNumbers(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+const teams = [
+  {
+    name: "team 1",
+    points: Math.ceil(Math.random() * 10),
+    fouls: Math.ceil(Math.random() * 10),
+  },
+  {
+    name: "team 2",
+    points: getRandomNumbers(1, 10),
+    fouls: getRandomNumbers(1, 10),
+  },
+  {
+    name: "team 3",
+    points: getRandomNumbers(1, 10),
+    fouls: getRandomNumbers(1, 10),
+  },
+  {
+    name: "team 4",
+    points: getRandomNumbers(1, 10),
+    fouls: getRandomNumbers(1, 10),
+  },
+  {
+    name: "team 5",
+    points: getRandomNumbers(1, 10),
+    fouls: getRandomNumbers(1, 10),
+  },
+];
+
+console.log(teams);
+
+const newTeams = []
+for (let i = 0; i < teams.length; i++) {
+    const team = teams[i]
+
+    console.log(team.name);
+    console.log(team.fouls);
+
+    newTeams.push({
+        name: team.name,
+        fouls: team.fouls
+    })
+}
+
+console.log(newTeams);
+
+    
+
+
